d00142efb9e7e5e88af326191d1ea54cb81be5eb
Max Campbell <max@max-laptop.maxc.in>
2017-11-29 16:08:34 +0000

    added redirect text

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ app.use(function(req, res, next) {
     console.log('%s %s %s', req.method, req.url, req.path);
     next();
 });
-app.get('iRoam/jobs/:jid/:job',(req,res)=>{
+app.get('/iRoam/jobs/:jid/:job',(req,res)=>{
     res.redirect(`https://maxc.ams3.digitaloceanspaces.com/jobs/${req.params.jid}/${req.params.jid}.html`)
+    res.send("redirecting")
 })
 app.listen(80)
