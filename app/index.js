dbef735557aef8ed3118c36114db35cdb35cc320
Max Campbell <max@max-laptop.maxc.in>
2017-11-29 16:02:00 +0000

    added job

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 let app = express()
 
-app.get('iRoam/jobs/:jid/*.html',(req,res)=>{
+app.get('iRoam/jobs/:jid/:job.html',(req,res)=>{
     res.redirect(`https://maxc.ams3.digitaloceanspaces.com/jobs/${req.params.jid}/${req.params.jid}.html`)
 })
 app.listen(80)
